5e753a3d471d24f920e4502d2cc49784e805683c
Gastón García Zavaleta <85880794+GastonGarciaZ@users.noreply.github.com>
2021-07-22 11:04:03 -0300

    Arreglo detalles

--- Tarea 5_Python y QGIS/Tarea 1/modelo4c.py
+++ Tarea 5_Python y QGIS/Tarea 1/modelo4c.py
@@ -1,12 +1,15 @@
+#########################################################################################
+####################                    Modelo 4C                    ####################
+#########################################################################################
+
+
 """
 Model exported as python.
 Name : modelo4c
 Group : 
 With QGIS : 31608
 """
-#########################################################################################
-####################                    Modelo 4C                    ####################
-#########################################################################################
+
 
 
 #########################################################################################
@@ -36,10 +39,10 @@ class Modelo4c(QgsProcessingAlgorithm):
         outputs = {}
 
         
-        #########################################################################################
-        ###                    Quitamos las variables que no vamos a usar                     ###                        
-        #########################################################################################
-        # Se eliminan las variables en 'COLUMN' de la capa en 'INPUT'. Capa resultado en 'OUTPUT'
+#########################################################################################
+###                    Quitamos las variables que no vamos a usar                     ###                        
+#########################################################################################
+# Se eliminan las variables en 'COLUMN' de la capa en 'INPUT'. Capa resultado en 'OUTPUT'
         
         alg_params = {
             'COLUMN': ['featurecla','scalerank','LABELRANK','SOVEREIGNT','SOV_A3','ADM0_DIF','LEVEL','TYPE','ADM0_A3','GEOU_DIF','GEOUNIT','GU_A3','SU_DIF','SUBUNIT','SU_A3','BRK_DIFF','NAME','NAME_LONG','BRK_A3','BRK_NAME','BRK_GROUP','ABBREV','POSTAL','FORMAL_EN','FORMAL_FR','NAME_CIAWF','NOTE_ADM0','NOTE_BRK','NAME_SORT','NAME_ALT','MAPCOLOR7','MAPCOLOR8','APCOLOR9','MAPCOLOR13','POP_EST','POP_RANK','GDP_MD_EST','POP_YEAR','LASTCENSUS','GDP_YEAR','ECONOMY','INCOME_GRP','WIKIPEDIA','FIPS_10_','ISO_A2','ISO_A3_EH','ISO_N3','UN_A3','WB_A2','WB_A3','WOE_ID','WOE_ID_EH','WOE_NOTE','ADM0_A3_IS','ADM0_A3_US','ADM0_A3_UN','ADM0_A3_WB','CONTINENT','REGION_UN','SUBREGION','REGION_WB','NAME_LEN','LONG_LEN','ABBREV_LEN','TINY','HOMEPART','MIN_ZOOM','MIN_LABEL','MAX_LABEL','NE_ID','WIKIDATAID','NAME_AR','NAME_BN','NAME_DE','NAME_EN','NAME_ES','NAME_FR','NAME_EL','NAME_HI','NAME_HU','NAME_ID','NAME_IT','NAME_JA','NAME_KO','NAME_NL','NAME_PL','NAME_PT','NAME_RU','NAME_SV','NAME_TR','NAME_VI','NAME_ZH','MAPCOLOR9','ADMIN_2','ISO_A3_2'],
@@ -52,14 +55,14 @@ class Modelo4c(QgsProcessingAlgorithm):
         feedback.setCurrentStep(1)
         if feedback.isCanceled():
             return {}
-        #########################################################################################
+#########################################################################################
         
         
-        ############################################################################
-        ###                Guardamos la base creada en formato csv               ###
-        ############################################################################
-        # Se guarda el archivo remanente ('INPUT') en formato .csv (pueden ser otros)              
-        # Se asigna la ruta y el nombre en 'OUTPUT
+#########################################################################################
+###                   Guardamos la base creada en formato .csv                      #####
+#########################################################################################
+# Se guarda el archivo remanente ('INPUT') en formato .csv (pueden ser otros)              
+# Se asigna la ruta y el nombre en 'OUTPUT
         
         alg_params = {
             'DATASOURCE_OPTIONS': '',
@@ -75,17 +78,17 @@ class Modelo4c(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
         
-        #########################################################################################
+#########################################################################################
         
         
         
-        #########################################################################################
-        ###             Calculamos la el area de cada poligono ('km2_area')                 #####
-        #########################################################################################
-        # nombre de la nueva variable 'FIELD_NAME'
-        # Capa sobre la cual realizar la variable 'INPUT'
-        # Capa resultado 'OUTPUT'
-        
+#########################################################################################
+###               Calculamos el area de cada poligono ('km2_area')                  #####
+#########################################################################################
+# nombre de la nueva variable en 'FIELD_NAME'
+# Capa sobre la cual realizar la variable en 'INPUT'
+# Capa resultado en 'OUTPUT'
+
         alg_params = {
             'FIELD_LENGTH': 10,
             'FIELD_NAME': 'km2area',
@@ -101,15 +104,15 @@ class Modelo4c(QgsProcessingAlgorithm):
         feedback.setCurrentStep(3)
         if feedback.isCanceled():
             return {}
-        #########################################################################################
+#########################################################################################
         
         
         
-        #########################################################################################
-        ###                             Corregimos las geometrías                             ###
-        #########################################################################################
-        # Es importante realizar este paso para evitar problemas como polígonos que se superponen
-        # o polígonos que no están cerrados.
+#########################################################################################
+###                             Corregimos las geometrías                             ###
+#########################################################################################
+# Es importante realizar este paso para evitar problemas como polígonos que se superponen
+# o polígonos que no están cerrados.
         
         alg_params = {
             'INPUT': 'Reproyectada_37e62b09_6b71_4457_a133_d4d68de8d39c',
@@ -121,13 +124,15 @@ class Modelo4c(QgsProcessingAlgorithm):
         feedback.setCurrentStep(4)
         if feedback.isCanceled():
             return {}
-        #########################################################################################
+#########################################################################################
 
         
-        #########################################################################################
-        ###                             Reproyectar capa                                      ###
-        #########################################################################################
-        # Sistema de coordenadas ESRI:54034- ('SOURCE_CRS')
+    
+#########################################################################################
+###                             Reproyectar capa                                      ###
+#########################################################################################
+# Sistema de coordenadas ESRI:54034- ('SOURCE_CRS')
+
         alg_params = {
             'INPUT': 'Campos_restantes_7ea0d45c_10b0_47ac_8cf1_79ecfc819704',
             'OPERATION': '',
@@ -137,7 +142,7 @@ class Modelo4c(QgsProcessingAlgorithm):
         outputs['ReproyectarCapa'] = processing.run('native:reprojectlayer', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         results['Countries_reprojected'] = outputs['ReproyectarCapa']['OUTPUT']
         return results
-        #########################################################################################
+#########################################################################################
     
     def name(self):
         return 'modelo4c'
